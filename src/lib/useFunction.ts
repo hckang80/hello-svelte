c442091905c6590b32e31c965989d62f132d9ef1
hckang80 <hckang80@gmail.com>
2022-04-14 19:53:07 +0900

    feat: Create storage composables

--- src/lib/useFunction.ts
+++ src/lib/useFunction.ts
@@ -1,3 +1,31 @@
+export const { localStorage: storage } = window
+
+export const getItem = (key) => {
+  try {
+    const value = storage.getItem(key)
+    return value ? JSON.parse(value) : ''
+  } catch {
+    return ''
+  }
+}
+
+export const setItem = (key, value) => {
+  try {
+    storage.setItem(key, JSON.stringify(value))
+  } catch {
+    throw new Error('Set not allowed')
+  }
+}
+
+export const removeItem = (key) => {
+  try {
+    storage.removeItem(key)
+  } catch {
+    throw new Error('Remove not allowed')
+  }
+}
+
+
 export const request = async<T> (url = '', options = {}): Promise<T> => {
   try {
     const response = await fetch(url, options)
